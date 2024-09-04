cd3a60eac0044692803ccd00521f00c51bde2600
Queensly K. Acheampongmaa <qacheampong@gmail.com>
2024-09-04 12:28:49 +0000

    Used the 'if', 'elif' and 'else' to create a simple game.

---
+++ toy_game.py
@@ -0,0 +1,8 @@
+print ('Choose a toy color')
+toy_color = input()
+if toy_color == 'red':
+    print("Let's play with the toy!")
+elif toy_color == 'green':
+    print('Show the toy to your friend!')
+else:
+    print("That's a nice color. Let's play with the " + toy_color + ' toy!')
