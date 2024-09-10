c460fa74be05acff8a255612f8a3bf3a24240498
Queensly K. Acheampongmaa <qacheampong@gmail.com>
2024-09-10 10:31:04 +0000

    Added a 'function' example

---
+++ functions_example.py
@@ -0,0 +1,16 @@
+#def hello(name):   #the variable inside the function is called Parameter
+   # print('Hello ' + name)
+    #print('Howdy!')
+    #print('Howdy!!!')
+    #print('Hello there!')
+
+
+
+#hello('Queensly')   #the value passed in the fucntion call is known as Argument
+#hello('Emmanuel')
+
+def plusOne(number):
+    return number + 1
+
+newnumber = plusOne(5)
+print(newnumber)
