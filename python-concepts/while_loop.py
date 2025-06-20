883dae6e8a89cdc9b8e31b69c2cba2edc77c4c6a
acheampongmaa <qacheampong@gmail.com>
2025-06-20 12:34:08 +0000

    example

--- python-concepts/while_loop.py
+++ python-concepts/while_loop.py
@@ -1,24 +1,25 @@
-#while statement
-# name = ''
-# while name != 'your name':
-#    print('Please type your name.')
-#    name = input()
-# print('Thank you!')    
+# Example 1 
+name = 'Olive'
+yourname = input('Please type your name:')
+while name != yourname:
+    print(f"It's not your name. Try again!")
+    break
+else: 
+    print(f"It's your name!")
 
-#break statement
-# name = ''
-# while True:
-#     print('Please type your name.')
-#     name = input()
-#     if name == 'your name':
-#         break
-# print('Thank you!')    
+#Example 2
+name = 'Queensly'
+while True:
+    yourname = input('Please type your name:')
+    if name == yourname:
+        break
+print('Thank you!')    
 
 
-# #continue statement
-# spam = 0
-# while spam < 5:
-#     spam = spam + 1
-#     if spam == 3:
-#         continue
-#     print ('spam is ' + str(spam))
+# Example 3
+spam = 0
+while spam < 5:
+    spam += 1
+    if spam == 3:
+     continue 
+    print ('spam is ' + str(spam))
