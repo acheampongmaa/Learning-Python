ef62607dff966309a20ee6349f6f375bc0f11b6e
acheampongmaa <qacheampong@gmail.com>
2025-06-20 11:47:25 +0000

    example added

--- python-concepts/loops.py
+++ python-concepts/loops.py
@@ -7,76 +7,76 @@ how many even numbers are in that range. Try it with:
 • range (5, 6) """
 
 #Example 1
-# count = 0
-# for i in range (5):
-#     if i % 2 == 0:
-#         print(i)
-#         count += 1
+count = 0
+for i in range (5):
+    if i % 2 == 0:
+        print(i)
+        count += 1
 
                
-# print(f"There are {count} even numbers in range 5!")
+print(f"There are {count} even numbers in range 5!")
 
 
 #Example 2
         
-# count = 0
-# for i in range (10):
-#     if i % 2 == 0:
-#         print(i)
-#         count += 1
+count = 0
+for i in range (10):
+    if i % 2 == 0:
+        print(i)
+        count += 1
 
                
-# print(f"There are {count} even numbers in range 10!")
+print(f"There are {count} even numbers in range 10!")
 
 #Example 3
-# count = 0
-# for i in range (2, 9, 3):
-#     if i % 2 == 0:
-#         print(i)
-#         count += 1
+count = 0
+for i in range (2, 9, 3):
+    if i % 2 == 0:
+        print(i)
+        count += 1
                
-# print(f"There are {count} even numbers in range specified!")
+print(f"There are {count} even numbers in range specified!")
 
 #Example 4
-# count = 0
-# for i in range (-4, 2, 6):
-#     if i % 2 == 0:
-#         print(i)
-#         count += i
+count = 0
+for i in range (-4, 2, 6):
+    if i % 2 == 0:
+        print(i)
+        count += i
                
-# print(f"There are {count} even numbers in range specified!")
+print(f"There are {count} even numbers in range specified!")
 
 # Example 5
-# count = 0
-# for i in range (5, 6):
-#     if i % 2 == 0:
-#         print(i)
-#         count += 1 
+count = 0
+for i in range (5, 6):
+    if i % 2 == 0:
+        print(i)
+        count += 1 
 
                
-# print(f"There are {count} even numbers in range specified!")
+print(f"There are {count} even numbers in range specified!")
 
 #Example 6
-# s = "abca"
-# unique_char = set(s)
-# for char in s:
-#     print(unique_char)
-#     count = len(unique_char)
-# print(unique_char)
+s = "abca"
+unique_char = set(s)
+for char in s:
+    print(unique_char)
+    count = len(unique_char)
+print(unique_char)
 
 #Example 7
-# s = "abca"
-# unique_char = ""
-# for char in s:
-#     # print (char)
-#     if char not in unique_char:
-#         unique_char = unique_char + char
-# print(len(unique_char))
+s = "abca"
+unique_char = ""
+for char in s:
+    # print (char)
+    if char not in unique_char:
+        unique_char = unique_char + char
+print(len(unique_char))
     
 #Example 
-# s = "abca"
-# count = 0
-# for char in s:
-#     if char in s:
-#         count +=1
-# print (len(count) )   
+s = "abca"
+count = 0
+for char in s:
+    if char in s:
+        count +=1
+print (len(count) )   
