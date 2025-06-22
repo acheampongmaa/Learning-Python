7e855470546e3cd0015bad569e2becba685588f0
acheampongmaa <qacheampong@gmail.com>
2025-06-22 16:00:28 +0000

    examples

--- python-concepts/for_loop.py
+++ python-concepts/for_loop.py
@@ -1,9 +1,7 @@
 #for loops , are used to loop a specific number of time.
-print('My name is')
-#for i in range (12,16):
-#for i in range(5):
-for i in range (0, 10):  #the 2 is a step meaning from 0, it should increase 2times to the next number.
-    print('Jimmy 5 times ' + str(i))
+# print('My name is')
+# # #for i in range (12,16):
+# # #for i in range(5):
+# # for i in range (0, 10):  #the 2 is a step meaning from 0, it should increase 2times to the next number.
+# print('Jimmy 5 times ' + str(i))
 
-
-    
--- python-concepts/functions_example.py
+++ python-concepts/functions_example.py
@@ -1,18 +1,18 @@
 #Example 1
-def hello(name):   #the variable inside the function is called Parameter
-   print('Hello ' + name)
+# def hello(name):   #the variable inside the function is called Parameter
+#    print('Hello ' + name)
     #print('Howdy!')
     #print('Howdy!!!')
     #print('Hello there!')
 
-hello('Queensly')   #the value passed in the fucntion call is known as Argument
+#hello('Queensly')   #the value passed in the fucntion call is known as Argument
 
 #Example 2
-def plusOne(number):
-    return number + 1
+# def plusOne(number):
+#     return number + 1
 
-newnumber = plusOne(5)
-print(newnumber)
+# newnumber = plusOne(5)
+# print(newnumber)
 
 #Example 3
 """ Write code that satisfies the following specs
@@ -23,11 +23,43 @@ Test your code with:
 • n 1O and d = 3
 • n = 195 and d = 13 """
 
-def div_by(n,d):
-   """ n and d are > 0"""
-   if d%n == 0:  #you can also try n%d
-      return True
-   else:
-      return False
-print(div_by (10, 3))   
-print(div_by (195, 13))
+# def div_by(n,d):
+#    """ n and d are > 0"""
+#    if d%n == 0:  #you can also try n%d
+#       return True
+#    else:
+#       return False
+# print(div_by (10, 3))   
+# print(div_by (195, 13))
+
+"""  
+ Write a function that meets these specs.
+def apply (criteria, n) :
+• criteria is any func like is_even that takes in a number and returns a bool
+• n is an int
+Returns how many ints from O to n (inclusive) match
+the criteria (i.e. return True when run with criteria)
+"""
+# def is_even(x):
+#     return x%2 == 0
+#print(is_even(4))
+       
+def is_five(x):
+    return x == 5
+
+def apply (criteria, n):
+   count = 0
+   for i in range(n+1):
+       if criteria(i):
+           #print(i)
+           count +=1
+   return count
+
+how_many = apply(is_even,5)
+print(how_many)
+
+how_many = apply(is_five,10)
+print(how_many)
+
+#Lambda functions
+
