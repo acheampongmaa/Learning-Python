c29130cf1914b55c2370b2a06b4130d154438525
acheampongmaa <qacheampong@gmail.com>
2025-06-23 15:36:22 +0000

    examples

--- python-concepts/functions_example.py
+++ python-concepts/functions_example.py
@@ -1,18 +1,18 @@
 #Example 1
-# def hello(name):   #the variable inside the function is called Parameter
-#    print('Hello ' + name)
+def hello(name):   #the variable inside the function is called Parameter
+   print('Hello ' + name)
     #print('Howdy!')
     #print('Howdy!!!')
     #print('Hello there!')
 
-#hello('Queensly')   #the value passed in the fucntion call is known as Argument
+hello('Queensly')   #the value passed in the fucntion call is known as Argument
 
 #Example 2
-# def plusOne(number):
-#     return number + 1
+def plusOne(number):
+    return number + 1
 
-# newnumber = plusOne(5)
-# print(newnumber)
+newnumber = plusOne(5)
+print(newnumber)
 
 #Example 3
 """ Write code that satisfies the following specs
@@ -23,14 +23,14 @@ Test your code with:
 • n 1O and d = 3
 • n = 195 and d = 13 """
 
-# def div_by(n,d):
-#    """ n and d are > 0"""
-#    if d%n == 0:  #you can also try n%d
-#       return True
-#    else:
-#       return False
-# print(div_by (10, 3))   
-# print(div_by (195, 13))
+def div_by(n,d):
+   """ n and d are > 0"""
+   if d%n == 0:  #you can also try n%d
+      return True
+   else:
+      return False
+print(div_by (10, 3))   
+print(div_by (195, 13))
 
 """  
  Write a function that meets these specs.
@@ -40,9 +40,9 @@ def apply (criteria, n) :
 Returns how many ints from O to n (inclusive) match
 the criteria (i.e. return True when run with criteria)
 """
-# def is_even(x):
-#     return x%2 == 0
-#print(is_even(4))
+def is_even(x):
+    return x%2 == 0
+print(is_even(4))
        
 def is_five(x):
     return x == 5
@@ -61,5 +61,21 @@ print(how_many)
 how_many = apply(is_five,10)
 print(how_many)
 
-#Lambda functions
+#Lambda functions are used once and can't be reused
 
+#Example 1
+
+""" Let's change the is_even fxn to a lambda fxn"""
+
+lambda x: x%2 == 0
+
+how_many = apply(lambda x: x%2 == 0, 6)
+print(how_many)
+
+#Example 2
+def do_twice(n, fn):
+     return fn(fn(n))
+
+""" fn is a fxn. A lambda fxn will be used """
+lambda x: x**2
+print(do_twice(5, lambda x: x**2))
