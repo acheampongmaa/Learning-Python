e6d763532c5d27a915eb6acc74612e7260569c62
acheampongmaa <qacheampong@gmail.com>
2025-06-25 10:02:17 +0000

    examples of tuples

--- python-concepts/tuples.py
+++ python-concepts/tuples.py
@@ -22,7 +22,7 @@ def char_counts(s):
       return ( v_count,c_count)
 
 
-print(char_counts('emmanuel'))
+# print(char_counts('emmanuel'))
 
 #  Method 2
 def char_counts(s):
@@ -39,8 +39,8 @@ def char_counts(s):
 print(char_counts('emmanuel'))
 
 
-""" Taking multiple arguments """
-#Method 1 tuple is used behind the scenes when * is used 
+# """ Taking multiple arguments """
+# #Method 1 tuple is used behind the scenes when * is used 
 def mean (*arg):
       total=0
       for a in arg:
@@ -57,3 +57,25 @@ def mean (arg):
       return total/len(arg)
 
 print(mean((1,2,2,4,6,7)))
+
+
+# Example 3
+"""
+• Write a function that meets these specs:
+• Use a loop and string methods to solve it.
+def count_digits_and_letters(s):
+    s is a string of any characters (letters, numbers, symbols)
+    Return a tuple where the first element is the
+    number of letters in s, and the second element
+    is the number of digits in s
+"""
+def count_digits_and_letters(s):
+      (d_count, l_count) = (0,0)
+      for elements in s:
+            if elements.isalpha():
+                  l_count += 1
+            elif elements.isdigit():
+                  d_count +=1
+      return   (d_count, l_count)   
+
+print(count_digits_and_letters("you890aregoing 456799jakals!~"))
